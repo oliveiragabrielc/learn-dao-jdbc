e1ce08389008faf8f0d94129cfc709a4452126f8
Gabriel Oliveira <gabriel.dev04@gmail.com>
2023-08-01 19:29:43 -0300

    update, delete and finById implementation

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -14,7 +14,14 @@ public class Program2 {
 
         Department department = new Department();
         department.setName("TI");
-
         departmentDao.insert(department);
+
+        Department department2 = new Department();
+        department2.setId(5);
+        department2.setName("Tecnologia da Informação");
+        departmentDao.update(department2);
+
+        Department department3 = departmentDao.findById(5);
+        departmentDao.deleteById(10);
     }
 }
--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -45,17 +45,62 @@ public class DepartmentDaoJDBC implements DepartmentDao {
 
     @Override
     public void update(Department obj) {
+        PreparedStatement st = null;
+        try{
+            st = conn.prepareStatement(
+                    "UPDATE department "
+                            +"SET Name = ? "
+                            + "WHERE Id = ?");
 
+            st.setString(1,obj.getName());
+            st.setInt(2,obj.getId());
+            st.executeUpdate();
+        }catch (SQLException e){
+            e.printStackTrace();
+        }finally {
+            DB.closeStatment(st);
+        }
     }
 
     @Override
     public void deleteById(Integer id) {
-
+            PreparedStatement st = null;
+            try{
+                st = conn.prepareStatement(
+                        "DELETE FROM department "
+                                +"WHERE Id = ?");
+                st.setInt(1,id);
+                st.executeUpdate();
+            }catch (SQLException e){
+                throw new DbException(e.getMessage());
+            }finally {
+                DB.closeStatment(st);
+            }
     }
 
     @Override
     public Department findById(Integer id) {
-        return null;
+        PreparedStatement st = null;
+        ResultSet rs = null;
+        try{
+            st = conn.prepareStatement(
+                    "SELECT * "
+                            + "FROM department "
+                            + "WHERE Id = ?"
+            );
+            st.setInt(1,id);
+            rs = st.executeQuery();
+            if(rs.next()){
+                Department dp = new Department(rs.getInt("Id"),rs.getString("Name"));
+                return dp;
+            }
+            return null;
+        }catch(SQLException e){
+            throw  new DbException(e.getMessage());
+        }finally {
+            DB.closeStatment(st);
+            DB.closeResultSet(rs);
+        }
     }
 
     @Override
