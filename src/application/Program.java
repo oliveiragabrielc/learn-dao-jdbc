f28ea284609f9e3fec4775c5a0ac217d9661fb00
Gabriel Oliveira <gabriel.dev04@gmail.com>
2023-08-01 11:52:04 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,17 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.time.LocalDate;
 
 public class Program {
     public static void main(String[] args) {
 
         Department obj = new Department(1,"Bola");
         System.out.println(obj);
+
+        Seller seller = new Seller(1,"Gabriel","gabriel@gmail.com", LocalDate.now(),3000.0,obj);
+        System.out.println(seller);
     }
 }
